09a51dab8c8fd563d0fab0e09fd5358a57177e6f
mianhuamu <147579631+mianhuamu@users.noreply.github.com>
2025-02-28 11:09:06 +0800

    Update 2.28修改版本.py

--- 2.18修改版本.py
+++ 2.18修改版本.py
@@ -35,8 +35,8 @@ class PythonSandbox:
         self.is_pro = False
         self.log_level = 1
         self.smoothed_divergence = 0.0
-        self.lp_factor = 0.05
-        self.div_factor = -2
+        self.lp_factor = 0.1
+        self.div_factor = -1
         self.real_obstacle_flag = 0
         self.use_harris = False
         self.old_gray = None
